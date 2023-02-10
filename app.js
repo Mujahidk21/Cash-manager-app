73471d33c84bb2ad298858c77e1b3ca04abf02df
Mujahidk21 <muja2k20@gmail.com>
2023-02-10 17:33:25 +0530

    changes made

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const idButton = document.querySelector("#id-button");
+
 const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount () {
     hidemessage();
     if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
+        if (billAmount.value > cashGiven.value) {
+            showMessage("you need to pay more !");
+        } else if (billAmount.value == cashGiven.value) {
+            showMessage("you are good to go !");
+        } else {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
-        } else {
-            showMessage("do you wanna wash plates?");
-        }
+ }
     } else {
-         showMessage("invalid");
-    }  
+        showMessage("Your bill amount should be greater than 0")
+    }
 });
 
 function calculateChange(amountToBeReturned) {
+    idButton.innerText = "Change to be returned " + amountToBeReturned;
     for (let i = 0; i < availableNotes.length; i++) {
         const numberofNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
         amountToBeReturned %= availableNotes[i];
