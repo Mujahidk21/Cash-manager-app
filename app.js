146cef1a81988e6f6213e7864b9534f618221a5a
Mujahidk21 <muja2k20@gmail.com>
2023-02-13 21:57:04 +0530

    del

--- app.js
+++ app.js
@@ -11,29 +11,13 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 // nextButton.addEventListener("click", handlenextButton);
 checkButton.addEventListener("click", validateBillAndCashAmount);
 
-//add next func later
-// function handlenextButton() {
-//     hidemessage();
-//     let billAmount = parseInt(billAmount);
-//     if (!Number.isInteger(billAmount)) {
-//         showMessage("enter valid amt");
-//     } else if (billAmount > 0) {
-//         document.querySelector(".cash-given").style.display = "block";
-//         document.querySelector(".check-button").style.display = "inline-block";
-//         nextButton.style.display = "none";
-//     } else if (billAmount > 0); {
-//         showMessage("bill  >  0");
-//     }
-// }
-
-
 function validateBillAndCashAmount() {
     hidemessage();
     if (billAmount.value > 0) {
-        if (billAmount.value > cashGiven.value) {
-            showMessage("you need to pay more ! Bill amt > Cash Given");
-        } else if (billAmount.value == cashGiven.value) {
+        if (billAmount.value == cashGiven.value) {
             showMessage("you are good to go ! Bill amt = Cash Given");
+        } else if (billAmount.value > cashGiven.value) {
+            showMessage("you need to pay more ! Bill amt > Cash Given");
         } else {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
